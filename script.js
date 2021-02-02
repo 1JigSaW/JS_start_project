34c9d508282f2b22dbd23ffd0cf3e06fc8e953d6
Jigsaw <example@gmail.com>
2021-02-02 17:04:06 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log('lalalala');
+console.log(null);
+console.log(undefined);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone.name);
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.png', 'apple.jpg']
+console.log(arr[1])
+
+// alert('Hello');
+
+// let answer = confirm('Are you here?');
+// console.log(answer);
+// let answer = prompt('Are you here?', 'Yes');
+console.log(typeof(answer));
+console.log(4 + ' answer');
+
+let incr = 10, decr = 10;
+// incr++;
+// decr--;
+// console.log(++incr)
+// console.log(--decr)
+
+console.log(incr++)
+console.log(decr--)
+
+console.log('2' == 2)
+console.log('2' === 2)
