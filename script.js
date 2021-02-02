ace3f07fba0eeda81cd4877af9fa484410c1380d
Jigsaw <example@gmail.com>
2021-02-02 18:45:34 +0300

    next lesson

--- script.js
+++ script.js
@@ -38,3 +38,45 @@ console.log(decr--)
 
 console.log('2' == 2)
 console.log('2' === 2)
+
+if (2*4 == 8) {
+    console.log("Verno")
+}
+
+let num = 50;
+if (num < 49) {
+    console.log("No!")
+} else if (num > 100) {
+    console.log("Many!")
+} else {
+    console.log('YES!')
+}
+
+(num == 50) ? console.log('YES!') : console.log('NO!')
+
+switch (num) {
+    case num < 49:
+        console.log('NO!');
+        break;
+    case num > 100:
+        console.log('Many!');
+        break;
+    case 50:
+        console.log('YEAAAAAAAAAS!');
+        break;
+}
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+do {
+    console.log(num);
+    num++;
+}
+while(num < 55)
+
+for(let i = 1;i < 8;i++) {
+    
+}
